5655e74d5523e38b9e2d25df828c5d14d1254eab
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-01-03 21:36:57 +0530

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
     return (
-        <div>
-            <h1>Words of Wonder Game</h1>
+        <div className="w-full h-dvh bg-gray-700 flex flex-col items-center justify-center">
+            <h1 className="text-4xl text-center font-bold text-gray-400"> Words of Wonder Game</h1>
         </div>
     );
 }
