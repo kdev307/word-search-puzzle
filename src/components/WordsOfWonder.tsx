715cd41e0c293cec7b9b10d79acd8031bc1ef55a
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-01-04 13:41:50 +0530

    added some more icons and styling corrections

--- src/components/WordsOfWonder.tsx
+++ src/components/WordsOfWonder.tsx
@@ -1,4 +1,11 @@
-import { LightBulbIcon, PaperAirplaneIcon, ShieldCheckIcon } from "@heroicons/react/20/solid";
+import {
+    ArrowPathIcon,
+    InformationCircleIcon,
+    LightBulbIcon,
+    PaperAirplaneIcon,
+    PlusIcon,
+    TrashIcon,
+} from "@heroicons/react/20/solid";
 import Button from "./Button";
 import Title from "./Title";
 import Grid from "./Grid";
@@ -6,35 +13,47 @@ import Grid from "./Grid";
 function WordsOfWonder() {
     return (
         <div className="w-full h-dvh bg-gray-800 flex flex-col items-center justify-center">
-            <Title as="h1" style="text-4xl text-center font-bold text-gray-200" className="mb-4">
-                Words of Wonder Game
-            </Title>
+            <div className="flex items-center justify-center gap-10 px-4 mb-10">
+                <Title as="h1" style="text-4xl text-center font-bold text-gray-200" className="">
+                    Words of Wonder
+                </Title>
 
-            <div className="flex items-center justify-evenly">
-                <Grid />
+                <InformationCircleIcon className="size-10 text-gray-200" />
             </div>
-
-            <div className="flex items-center justify-center gap-10 px-10 py-5 w-3/4">
-                <Button
-                    icon={<ShieldCheckIcon className="size-8" />}
-                    text="Validate Word"
-                    style="bg-gray-700 text-white w-full"
-                    onClick={() => alert("Validate")}
-                />
-
-                <Button
-                    icon={<PaperAirplaneIcon className="size-8" />}
-                    text="Finish"
-                    style="bg-gray-700 text-white w-full"
-                    onClick={() => alert("Finish")}
-                />
-
-                <Button
-                    icon={<LightBulbIcon className="size-8" />}
-                    text="Hint"
-                    style="bg-gray-700 text-white w-full"
-                    onClick={() => alert("Hint")}
-                />
+            <div className="flex items-center justify-evenly gap-10">
+                <Grid />
+                <div className="flex flex-col items-center justify-center gap-10 px-10 py-5 w-3/4">
+                    <Button
+                        icon={<PlusIcon className="size-8" />}
+                        text="New Game"
+                        style="bg-gray-700 text-white w-full"
+                        onClick={() => alert("New Game")}
+                    />
+                    <Button
+                        icon={<ArrowPathIcon className="size-8" />}
+                        text="Restart Game"
+                        style="bg-gray-700 text-white w-full"
+                        onClick={() => alert("Restart Game")}
+                    />
+                    <Button
+                        icon={<TrashIcon className="size-8" />}
+                        text="Clear Selection"
+                        style="bg-gray-700 text-white w-full"
+                        onClick={() => alert("Clear")}
+                    />
+                    <Button
+                        icon={<PaperAirplaneIcon className="size-8" />}
+                        text="Finish Game"
+                        style="bg-gray-700 text-white w-full"
+                        onClick={() => alert("Finish")}
+                    />
+                    <Button
+                        icon={<LightBulbIcon className="size-8" />}
+                        text="Need Help"
+                        style="bg-gray-700 text-white w-full"
+                        onClick={() => alert("Hint")}
+                    />
+                </div>
             </div>
         </div>
     );
