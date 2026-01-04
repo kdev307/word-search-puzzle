7b28575b121c64c77408da2bfce2014f9f6856bf
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-01-04 13:38:44 +0530

    created atoms of the project

---
+++ src/components/Cell.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type CellProps = {
+    children?: React.ReactNode;
+};
+
+function Cell({ children }: CellProps) {
+    return (
+        <div className="p-5 border-2 rounded-2xl text-center text-2xl font-bold bg-gray-200 text-gray-900">
+            {children}
+        </div>
+    );
+}
+
+export default Cell;
