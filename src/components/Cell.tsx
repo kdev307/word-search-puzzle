0403f9be8e3612c4aca698c42b64664998d47d16
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-01-10 21:24:35 +0530

    minor cell styling correction

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -11,11 +11,11 @@ type CellProps = {
 function Cell({ children, selected, found, color, rounded, ...props }: CellProps) {
     return (
         <div
-            className={`flex aspect-square min-w-14 items-center justify-center overflow-clip p-2 text-center text-xl font-bold transition-all duration-150 select-none ${rounded || ''} ${
+            className={`relative flex aspect-square min-w-14 items-center justify-center overflow-clip text-center text-xl font-bold transition-all duration-150 select-none ${rounded || ''} ${
                 selected ? 'bg-gray-800 text-white' : 'text-gray-900 hover:bg-gray-300'
             } ${found && 'text-white'} `}
             {...props}
-            style={found ? { backgroundColor: color, borderColor: color } : {}}
+            style={found ? { backgroundColor: color } : {}}
         >
             {children}
         </div>
