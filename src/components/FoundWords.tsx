b916ca3b20e58977026b6e744564b404cbcdfcd4
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-01-10 17:10:41 +0530

    highlihgting the found word in the list

--- src/components/FoundWords.tsx
+++ src/components/FoundWords.tsx
@@ -2,19 +2,24 @@ import useWOW from '../hooks/useWOW';
 import Title from './Title';
 
 function FoundWords() {
-    const { words } = useWOW();
+    const { words, foundWords } = useWOW();
     return (
         <div className="flex h-full w-full flex-col items-center gap-4 rounded-2xl bg-gray-600 px-6 py-4 text-gray-100 shadow-xl">
             <Title as="h2" style="text-2xl font-bold text-gray-200 tracking-wide">
-                Find These Words
+                Words to Find ({foundWords.length}/{words.length})
             </Title>
 
             <ul className="grid w-full grid-cols-1 gap-3">
                 {words.map((word) => {
+                    const isFound = foundWords.includes(word);
                     return (
                         <li
                             key={word}
-                            className="rounded-xl bg-gray-700 px-4 py-2 text-center text-xl font-semibold text-gray-300 transition-all duration-200 hover:bg-gray-200 hover:text-gray-800"
+                            className={`rounded-xl px-4 py-2 text-center text-xl font-semibold transition-all duration-200 ${
+                                isFound
+                                    ? 'animate-pulse bg-gray-900 text-gray-100'
+                                    : 'bg-gray-700 text-gray-300 hover:bg-gray-200 hover:text-gray-800'
+                            } `}
                         >
                             {word.toUpperCase()}
                         </li>
