763a38f6c7446944d2f82ca4b5bc6a6459a3ef81
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-01-11 15:03:01 +0530

    updated cursor

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type CellProps = {
     children?: React.ReactNode;
@@ -9,10 +9,16 @@ type CellProps = {
 };
 
 function Cell({ children, selected, found, color, rounded, ...props }: CellProps) {
+    useEffect(() => {
+        document.body.style.cursor = selected ? 'grabbing' : '';
+        return () => {
+            document.body.style.cursor = '';
+        };
+    }, [selected]);
     return (
         <div
-            className={`relative flex aspect-square min-w-14 items-center justify-center overflow-clip text-center text-xl font-bold transition-all duration-150 select-none ${rounded || ''} ${
-                selected ? 'bg-gray-800 text-white' : 'text-gray-900 hover:bg-gray-300'
+            className={`relative flex aspect-square min-w-14 cursor-pointer items-center justify-center overflow-clip text-center text-xl font-bold transition-all duration-150 select-none ${rounded || ''} ${
+                selected ? 'cursor-grab bg-gray-800 text-white' : 'text-gray-900 hover:bg-gray-300'
             } ${found && 'text-white'} `}
             {...props}
             style={found ? { backgroundColor: color } : {}}
