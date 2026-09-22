9070d70fb22c21a682c38ce1203a7f3393aed556
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-09-22 19:08:59 +0530

    configured project for deployment via gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: "/word-search-puzzle/",
+    // for testing in local environment
     server: {
         host: '0.0.0.0',
     },
