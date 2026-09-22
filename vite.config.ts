3ffb6fd1b87ea50ee5f45c5b971a81ae6bc5f18f
kdev307 <127479436+kdev307@users.noreply.github.com>
2026-09-22 19:31:25 +0530

    updated vite config for separting dev and production mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react(), tailwindcss()],
-    base: "/word-search-puzzle/",
+    base: mode === "production" ? "/word-search-puzzle/" : "/",
     // for testing in local environment
     server: {
         host: '0.0.0.0',
     },
-});
+}));
